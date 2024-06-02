69f602cfa35bbec19662562638301da7dfaa7e50
Vladimir Shetnikov <vladimir@kinestex.com>
2024-06-02 18:49:16 +0400

    Initial Commit

---
+++ KinesteXSDKDemoSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KinesteXSDKDemoSwift
+//
+//  Created by Vladimir Shetnikov on 6/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KinesteXSDKDemoSwift/KinesteXSDKDemoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  KinesteXSDKDemoSwiftApp.swift
+//  KinesteXSDKDemoSwift
+//
+//  Created by Vladimir Shetnikov on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct KinesteXSDKDemoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
